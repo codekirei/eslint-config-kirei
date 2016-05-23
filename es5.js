40124e7e2c52562f51ecb5f01ba0eceeee0926fd
Jacob Blakely <jacob@codekirei.com>
2016-05-22 21:59:54 -0700

    import configs

--- es5.js
+++ es5.js
@@ -1,1 +1,24 @@
-// es5 rules
+'use strict'
+
+// modules ---------------------------------------------------------------------
+
+const bestPractices = require('./rules/best-practices')
+const node = require('./rules/node')
+const possibleErrors = require('./rules/possible-errors')
+const strictMode = require('./rules/strict-mode')
+const stylisticIssues = require('./rules/stylistic-issues')
+const variables = require('./rules/variables')
+
+// config ----------------------------------------------------------------------
+
+module.exports = {
+  rules: Object.assign(
+    {},
+    bestPractices,
+    node,
+    possibleErrors,
+    strictMode,
+    stylisticIssues,
+    variables
+  ),
+}
--- index.js
+++ index.js
@@ -1,1 +1,47 @@
-// es6 rules
+'use strict'
+
+// modules ---------------------------------------------------------------------
+
+// npm
+const es5Rules = require('eslint-config-kirei-es5').rules
+
+// local
+const es6Rules = require('./rules/es6')
+const importRules = require('./rules/import')
+
+// config ----------------------------------------------------------------------
+
+module.exports = {
+  parserOptions: {
+    ecmaVersion: 6,
+    ecmaFeatures: {
+      impliedStrict: true,
+      jsx: true,
+    },
+  },
+  plugins: [
+    'import',
+  ],
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: [
+          '.js',
+          '.json',
+          '.jsx',
+        ],
+      },
+    },
+  },
+  env: {
+    es6: true,
+  },
+  rules: Object.assign(
+    {},
+    es5Rules,
+    es6Rules,
+    importRules
+  ),
+  es6Rules,
+  importRules,
+}
